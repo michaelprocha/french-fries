17bdd3803a7f60dca9ae103ebbaff41b3924a533
michaelprocha <michaelrocha9722@gmail.com>
2025-09-03 08:45:01 -0300

    feat: finalize header

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,19 @@
 (function () {
-    const menu = document.getElementById("menu");
-    const modal = document.getElementById("modal");
-    console.log(modal);
-    menu.addEventListener("click", () => {
-        modal.classList.toggle("menu-modal");
-    });
+	const menu = document.getElementById("menu");
+	const modal = document.getElementById("modal");
+	const clone = document.getElementById("close");
+	const menuNavegation = document.getElementById("menu-navegation");
+
+	console.log(modal);
+	menu.addEventListener("click", () => {
+		modal.classList.toggle("menu-modal--disabled");
+		menuNavegation.classList.toggle("menu-navegation--disabled");
+		menu.classList.toggle("menu--disabled");
+	});
+
+	clone.addEventListener("click", () => {
+		modal.classList.toggle("menu-modal--disabled");
+		menuNavegation.classList.toggle("menu-navegation--disabled");
+		menu.classList.toggle("menu--disabled");
+	});
 })();
